1972ad648ecf9ebbd32414d087ce3c7ee9fe1f48
contractoriq-wyk <wyktrckllc@gmail.com>
2026-05-20 18:57:02 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { CartProvider } from "./cart/CartContext.jsx";
+import { CartProvider }     from "./cart/CartContext.jsx";
 import { LanguageProvider } from "./lang/LanguageContext.jsx";
+import { AdminProvider }    from "./admin/AdminContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <LanguageProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
+      <AdminProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </AdminProvider>
     </LanguageProvider>
   </StrictMode>
 );
